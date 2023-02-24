de9d55e1bdb667a4836b740e1fa0ad45e9518ac9
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-02-24 15:56:12 +0900

    project test

--- test/js/main.js
+++ test/js/main.js
@@ -1,19 +1,33 @@
 const header = document.querySelector("header");
 
-const video = document.querySelector("video");
-video.setAttribute("style","opacity: 0; transition: 3s" );
+// const video = document.querySelector("video");
+// video.setAttribute("style","opacity: 0; transition: 10s" );
 
-// window.onload = function(){
-//     video.setAttribute("style","opacity: 0.5; transition: 5s" );
-// }
-video.addEventListener("play", function(){
-    console.log("재생 시작");
+// // window.onload = function(){
+// //     video.setAttribute("style","opacity: 0.5; transition: 5s" );
+// // }
+
+// video.addEventListener("play", function(){
+//     console.log("재생 시작");
     
     
+// })
+
+// video.addEventListener("pause", function(){
+//     console.log("재생 중단");
+//     video.classList.add("hide");
+//     header.classList.remove("hide");
+// })
+
+
+const modal = document.getElementById("modal")
+const btnModal = document.getElementById("btn-modal")
+btnModal.addEventListener("click", e => {
+    modal.style.display = "flex"
 })
 
-video.addEventListener("pause", function(){
-    console.log("재생 중단");
-    video.classList.add("hide");
-    header.classList.remove("hide");
+
+const closeBtn = modal.querySelector(".close-area")
+closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
 })
